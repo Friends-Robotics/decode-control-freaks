4321a1eb44dc81de8d97cea7891f07c4794d0fa6
Harry <harryseymour.ulster@gmail.com>
2025-09-05 22:09:56 +0100

    Day1: Chapters 1 to 3

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAndNotes/IfOpMode.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.TestAndNotes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class IfOpMode extends OpMode {
+
+    @Override
+    public void init(){}
+
+    @Override
+    public void loop(){
+        if(gamepad1.left_stick_y < 0){
+            telemetry.addData("Left Stick", " is negative");
+            telemetry.addData("Left sticky", gamepad1.left_stick_y);
+        }
+    }
+}
