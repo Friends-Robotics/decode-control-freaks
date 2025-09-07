da47d4066cbf9fdaddd52924e751367a6fa731d9
Harry <harryseymour.ulster@gmail.com>
2025-09-07 14:07:49 +0100

    Chapter 7

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAndNotes/Motors/Mechanisms/ProgrammingBoard3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAndNotes/Motors/Mechanisms/ProgrammingBoard3.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.TestAndNotes.Motors;
+package org.firstinspires.ftc.teamcode.TestAndNotes.Motors.Mechanisms;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
@@ -14,11 +14,17 @@ public class ProgrammingBoard3{
         touchSensor.setMode(DigitalChannel.Mode.INPUT);
 
         motor = hwMap.get(DcMotor.class, "motor");
-        // This assigns to the variable motor the hardware that is in the configuration file of type
+        // This assigns to the variable motor to the hardware that is in the configuration file of type
         // DcMotor.class and with the name of motor.
         // This name has to match exactly what is in config file
 
         motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // This sets how we want to use the motor
+        // It used the encoder on the motor so that we are setting a speed and it figures out how to
+        // modify power to get to that speed.
+
+        // We like this because if we set two motors to the same speed then they have a better chance
+        // at being the same speed than any other mode
     }
     public boolean isTouchSensorPressed(){
         return !touchSensor.getState();
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAndNotes/Motors/OpModes/MotorOpMode2.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.TestAndNotes.Motors.OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.TestAndNotes.Motors.Mechanisms.ProgrammingBoard4;
+
+@TeleOp
+public class MotorOpMode2 extends OpMode {
+    ProgrammingBoard4 board = new ProgrammingBoard4();
+
+    @Override
+    public void init(){
+        board.init(hardwareMap);
+    }
+
+    @Override
+    public void loop(){
+        board.setMotorSpeed(0.5);
+        telemetry.addData("Motor Rotations", board.getMotorRotations());
+    }
+}
